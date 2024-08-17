107aa3848e3b3045890f1eae9887cdb38eaa449c
Igor Usenko <usenkoigor01@gmail.com>
2024-08-17 18:09:33 +0300

    Обновление версии и переработка интерфейса.

--- llm.py
+++ llm.py
@@ -1,20 +1,19 @@
 import os
 import re
-import keyboard
 import requests
 import webbrowser
 import datetime
-import tkinter as tk
 from webscout import KOBOLDAI, BLACKBOXAI, ThinkAnyAI, PhindSearch, DeepInfra, Julius, WEBS as w
 from freeGPT import Client
-from tkinter import ttk
+import customtkinter as ctk
+import tkinter as tk
 from datetime import datetime
 from tkinter import messagebox
 from PIL import Image
 from io import BytesIO
 from packaging import version
 
-CURRENT_VERSION = "1.19"
+CURRENT_VERSION = "1.20"
 
 prompt = """###INSTRUCTIONS###
 
@@ -169,24 +168,24 @@ model_functions = {
                 "BlackboxAI": communicate_with_BlackboxAI,
                 "Claude-3-haiku(ThinkAny)": lambda user_input: communicate_with_ThinkAnyAI(user_input, "claude-3-haiku"),
                 "Claude-3-haiku(DDG)": lambda user_input: communicate_with_DuckDuckGO(user_input, "claude-3-haiku"),
-                "Nemotron-4-340B-Instruct": lambda user_input: communicate_with_DeepInfra(user_input, "nvidia/Nemotron-4-340B-Instruct"),
-                "Qwen2-72B-Instruct": lambda user_input: communicate_with_DeepInfra(user_input, "Qwen/Qwen2-72B-Instruct"),
+                # "Nemotron-4-340B-Instruct": lambda user_input: communicate_with_DeepInfra(user_input, "nvidia/Nemotron-4-340B-Instruct"),
+                # "Qwen2-72B-Instruct": lambda user_input: communicate_with_DeepInfra(user_input, "Qwen/Qwen2-72B-Instruct"),
                 "Phind": communicate_with_Phind,
-                "Llama-70b (DDG)": lambda user_input: communicate_with_DuckDuckGO(user_input, "llama-3-70b"),
+                "Llama 3-70b (DDG)": lambda user_input: communicate_with_DuckDuckGO(user_input, "llama-3-70b"),
                 "Llama-3.1-8b-instruct": lambda user_input: communicate_with_ThinkAnyAI(user_input,"llama-3.1-8b-instruct"),
-                "Llama-3.1-70b (DeepInfra)": lambda user_input: communicate_with_DeepInfra(user_input, "meta-llama/Meta-Llama-3.1-70B-Instruct"),
-                "Meta-Llama-3.1-405B-Instruct": lambda user_input: communicate_with_DeepInfra(user_input, "meta-llama/Meta-Llama-3.1-405B-Instruct"),
+                # "Llama-3.1-70b (DeepInfra)": lambda user_input: communicate_with_DeepInfra(user_input, "meta-llama/Meta-Llama-3.1-70B-Instruct"),
+                # "Meta-Llama-3.1-405B-Instruct": lambda user_input: communicate_with_DeepInfra(user_input, "meta-llama/Meta-Llama-3.1-405B-Instruct"),
                 "Gemini-pro": lambda user_input: communicate_with_ThinkAnyAI(user_input, "gemini-pro"),
                 "Gemma-2-27b-it": lambda user_input: communicate_with_ThinkAnyAI(user_input, "google/gemma-2-27b-it"),
                 "Mistral-7b-instruct": lambda user_input: communicate_with_ThinkAnyAI(user_input,"mistral-7b-instruct"),
                 "Mixtral-8x7b": lambda user_input: communicate_with_DuckDuckGO(user_input,"mixtral-8x7b"),
-                "Mixtral-8x22B-Instruct-v0.1": lambda user_input: communicate_with_DeepInfra(user_input,"mistralai/Mixtral-8x22B-Instruct-v0.1"),
-                "WizardLM-2-8x22B": lambda user_input: communicate_with_DeepInfra(user_input,"microsoft/WizardLM-2-8x22B"),
-                "Mixtral-8x7B-Instruct-v0.1": lambda user_input: communicate_with_DeepInfra(user_input,"mistralai/Mixtral-8x7B-Instruct-v0.1"),
-                "Dolphin-2.6-mixtral-8x7b": lambda user_input: communicate_with_DeepInfra(user_input,"cognitivecomputations/dolphin-2.6-mixtral-8x7b"),
-                "Dolphin-2.9.1-llama-3-70b": lambda user_input: communicate_with_DeepInfra(user_input,"cognitivecomputations/dolphin-2.9.1-llama-3-70b"),
-                "L3-70B-Euryale-v2.1": lambda user_input: communicate_with_DeepInfra(user_input,"Sao10K/L3-70B-Euryale-v2.1"),
-                "Phi-3-medium-4k-instruct": lambda user_input: communicate_with_DeepInfra(user_input,"microsoft/Phi-3-medium-4k-instruct"),
+                # "Mixtral-8x22B-Instruct-v0.1": lambda user_input: communicate_with_DeepInfra(user_input,"mistralai/Mixtral-8x22B-Instruct-v0.1"),
+                # "WizardLM-2-8x22B": lambda user_input: communicate_with_DeepInfra(user_input,"microsoft/WizardLM-2-8x22B"),
+                # "Mixtral-8x7B-Instruct-v0.1": lambda user_input: communicate_with_DeepInfra(user_input,"mistralai/Mixtral-8x7B-Instruct-v0.1"),
+                # "Dolphin-2.6-mixtral-8x7b": lambda user_input: communicate_with_DeepInfra(user_input,"cognitivecomputations/dolphin-2.6-mixtral-8x7b"),
+                # "Dolphin-2.9.1-llama-3-70b": lambda user_input: communicate_with_DeepInfra(user_input,"cognitivecomputations/dolphin-2.9.1-llama-3-70b"),
+                # "L3-70B-Euryale-v2.1": lambda user_input: communicate_with_DeepInfra(user_input,"Sao10K/L3-70B-Euryale-v2.1"),
+                # "Phi-3-medium-4k-instruct": lambda user_input: communicate_with_DeepInfra(user_input,"microsoft/Phi-3-medium-4k-instruct"),
                 "Prodia_img":lambda user_input: gen_img(user_input, "prodia"),
                 "Pollinations_img":lambda user_input: gen_img(user_input, "pollinations")}
 
@@ -207,107 +206,120 @@ def gen_img(user_input, model):
     except Exception as e:
        return f"Ошибка при генерации картинки: {e}"
 
-class ChatApp(tk.Tk):
+class ChatApp(ctk.CTk):
     def __init__(self):
         try:
             super().__init__()
+            ctk.set_appearance_mode("dark")
+            ctk.set_default_color_theme("green")
             self.title("AI Chat")
             self.geometry("{}x{}+0+0".format(self.winfo_screenwidth(), self.winfo_screenheight()))
-            self.configure(bg="black")
 
             # Создание виджета chat_history с прокруткой
-            self.chat_history_frame = tk.Frame(self, bg="black")
+            self.chat_history_frame = ctk.CTkFrame(self)
             self.chat_history_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
-            self.chat_history_scrollbar = tk.Scrollbar(self.chat_history_frame)
-            self.chat_history_scrollbar.pack(side="right", fill="y")
-
-            self.chat_history = tk.Text(self.chat_history_frame, bg="black", fg="green", font=("Consolas", 14),
-                                        yscrollcommand=self.chat_history_scrollbar.set)
+            self.chat_history = ctk.CTkTextbox(self.chat_history_frame, font=("Consolas", 18))
             self.chat_history.pack(fill="both", expand=True)
-
-            self.chat_history_scrollbar.config(command=self.chat_history.yview)
             self.chat_history.configure(state="disabled")
-
-            # Настройка стилей виджета chat_history
-            self.chat_history.tag_configure("response", foreground="yellow")
-            self.chat_history.tag_configure("user_input", foreground="red")
-
 
             # Добавление контекстного меню
             self.chat_history.bind("<Button-3>", self.show_context_menu)
             self.context_menu = tk.Menu(self, tearoff=0)
             self.context_menu.add_command(label="Копировать", command=self.copy_text)
 
-            self.input_frame = tk.Frame(self, bg="black")
+            self.input_frame = ctk.CTkFrame(self)
             self.input_frame.pack(fill="x", padx=10, pady=10)
 
-            self.model_label = ttk.Label(self.input_frame, text="Выберите модель:", foreground="green", background="black", font=("Consolas", 14))
+            # Метка
+            self.model_label = ctk.CTkLabel(self.input_frame, text="Выберите модель:", font=("Consolas", 18))
             self.model_label.pack(side="left", padx=5)
 
+            # Комбобокс
             self.model_var = tk.StringVar()
-            self.model_combobox = ttk.Combobox(self.input_frame, textvariable=self.model_var, values=list(model_functions.keys()), state="readonly")
+            self.model_combobox = ctk.CTkOptionMenu(self.input_frame, variable=self.model_var, font=("Consolas", 16),
+                                                    values=list(model_functions.keys()))
             self.model_combobox.pack(side="left", padx=5)
-            self.model_combobox.current(0) # Модель по умолчанию
+            self.model_combobox.set(list(model_functions.keys())[0])  # Модель по умолчанию
 
-            self.input_entry = tk.Text(self.input_frame, bg="black", fg="green", insertbackground="green", font=("Consolas", 14), height=10, width=50, wrap="word", undo=True, autoseparators=True, maxundo=-1)
+            # Кнопка переключения темы
+            self.theme_button = ctk.CTkButton(self.input_frame, text="Светлая тема", command=self.toggle_theme,
+                                               font=("Consolas", 14), text_color="white")
+            self.theme_button.pack(side="right", pady=(0,93))
+
+            self.input_entry = ctk.CTkTextbox(self.input_frame, font=("Consolas", 16), height=120, width=150, wrap="word", text_color="orange")
             self.input_entry.pack(side="left", fill="x", expand=True, padx=5)
-            self.input_entry.configure(state="normal")
 
             # Горячие клавиши
             self.input_entry.bind("<Shift-Return>", self.insert_newline)
             self.input_entry.bind("<Return>", self.send_message)
-            keyboard.add_hotkey("ctrl+z", self.undo_input)
-            keyboard.add_hotkey("ctrl+c", self.copy_text)
-            keyboard.add_hotkey("ctrl+v", self.paste_text)
-            keyboard.add_hotkey("ctrl+a", self.select_all)
+            self.input_entry.bind("ctrl+z", self.undo_input)
+            self.input_entry.bind("ctrl+c", self.copy_text)
+            self.input_entry.bind("ctrl+v", self.paste_text)
+            self.input_entry.bind("ctrl+a", self.select_all)
 
-            # Настройка стилей виджета chat_history
-            self.chat_history.tag_configure("response", foreground="yellow")
-            self.chat_history.tag_configure("user_input", foreground="orange")
-            self.chat_history.tag_configure("system_line", foreground="cyan")
             # Кнопки
-            self.send_button = tk.Button(self.input_frame, text="Отправить", bg="black", fg="green", font=("Consolas", 14),
-                                         command=self.send_message, borderwidth=0)
+            self.send_button = ctk.CTkButton(self.input_frame, text="Отправить", command=self.send_message,
+                                               font=("Consolas", 14), text_color="white")
             self.send_button.pack(side="top", padx=5, pady=10)
 
-            self.clear_button = tk.Button(self.input_frame, text="Очистить чат", bg="black", fg="green",
-                                          font=("Consolas", 14), command=self.clear_chat, borderwidth=0)
+            self.clear_button = ctk.CTkButton(self.input_frame, text="Очистить чат", command=self.clear_chat,
+                                                font=("Consolas", 14), text_color="white")
             self.clear_button.pack(side="top", padx=5, pady=15)
 
+            # Определение тегов для цветного текста
+            self.chat_history.tag_add("user_input", "1.0")
+            self.chat_history.tag_add("response", "1.0")
+            self.chat_history.tag_add("system_line", "1.0")
+
+            # Определение тегов для цветного текста
+            self.chat_history.tag_config("user_input", foreground="orange")
+            self.chat_history.tag_config("response", foreground="yellow")
+            self.chat_history.tag_config("system_line", foreground="cyan")
+
             # Чекбокс "Вести историю"
-            style = ttk.Style()
-            style.configure("CustomCheckbutton.TCheckbutton",
-                            font=("Consolas", 14),
-                            background="black",
-                            foreground="yellow",
-                            relief="flat")
-
-            input_frame = tk.Frame(self, bg="black")
-            input_frame.pack()
-
             self.history_var = tk.BooleanVar()
-            self.history_checkbox = ttk.Checkbutton(
-                input_frame,
-                text="Вести историю",
-                variable=self.history_var,
-                onvalue=True,
-                offvalue=False,
-                style="CustomCheckbutton.TCheckbutton"
-            )
+            self.history_checkbox = ctk.CTkCheckBox(self.input_frame, text="Вести историю", variable=self.history_var)
             self.history_checkbox.pack(side="top", padx=5, pady=5)
 
         except Exception as e:
             messagebox.showerror("Возникла ошибка", e)
 
-    def undo_input(self, event=None):
-        """
-        Отменяет последнее действие ввода в поле ввода.
-        """
+    def send_message(self, event=None):
         try:
-            self.input_entry.edit_undo()
+            user_input = self.input_entry.get("1.0", "end-1c")
+            # Добавляем сообщение "Ожидайте" в поле ввода
+            self.input_entry.delete("1.0", "end-1c")  # Сначала очищаем поле ввода
+            self.input_entry.insert("1.0", "Ожидайте...", "system_line")  # Вставляем сообщение "Ожидайте"
+            user_input = user_input.replace('\n', '')  # Удаляем символы новой строки
+            user_input = user_input.strip()  # Удаляем пробелы в начале и конце строки
+            if user_input:
+                model = self.model_var.get()
+
+                if model in model_functions:
+                    self.chat_history.configure(state="normal")
+                    self.chat_history.insert(tk.END, f"Вы: {user_input}\n", "user_input")
+
+                    response = model_functions[model](user_input)
+                    self.chat_history.insert(tk.END, f"\nОтвет от {model}: {response}\n", "response")
+                    self.chat_history.insert(tk.END, 155 * "=", "system_line")
+                    self.chat_history.insert(tk.END, "\n", "system_line")
+                    self.chat_history.configure(state="disabled")
+
+                    if self.history_var.get():
+                        self.write_history(user_input, response)
+
+                    self.input_entry.delete("1.0", "end-1c")  # Очистка поля ввода
         except Exception as e:
-            pass
+            messagebox.showerror("Возникла ошибка", e)
+
+    def undo_input(self, event=None):
+        """Функция для отмены последнего действия в поле ввода."""
+        try:
+            self.input_entry.configure(state="normal")
+            self.input_entry.edit_undo()  # Отменяем последнее действие
+        except Exception as e:
+            messagebox.showerror("Ошибка", str(e))
 
     def on_key_press(self, event):
         if event.keysym == "a" and event.state & 0x4:  # CTRL + a
@@ -353,14 +365,6 @@ class ChatApp(tk.Tk):
         except Exception as e:
             messagebox.showerror("Возникла ошибка", e)
 
-    def remove_newline(self, event):
-        try:
-            text = self.input_entry.get("1.0", "end-1c")
-            self.input_entry.delete("1.0", "end")
-            self.input_entry.insert("1.0", text.rstrip("\n"))
-        except Exception as e:
-            messagebox.showerror("Возникла ошибка", e)
-
     def check_input(self, event):
         try:
             if self.input_entry.get("1.0", "end-1c").strip() == "":
@@ -374,28 +378,6 @@ class ChatApp(tk.Tk):
             text = f"Дата и время: {now.strftime('%d.%m.%Y %H:%M:%S')}\nЗапрос пользователя: {user_input}\nОтвет ИИ: {response}\n\n{100*"="}\n"
             with open("llm_history.txt", "a") as f:
                 f.write(text)
-        except Exception as e:
-            messagebox.showerror("Возникла ошибка", e)
-
-    def send_message(self, event=None):
-        try:
-            user_input = self.input_entry.get("1.0", "end-1c").strip()
-            if user_input:
-                model = self.model_var.get()
-
-                if model in model_functions:
-                    self.chat_history.configure(state="normal")
-                    self.chat_history.insert(tk.END, f"Вы: {user_input}\n","user_input")
-                    response = model_functions[model](user_input)
-                    self.chat_history.insert(tk.END, f"\nОтвет от {model}: {response}\n","response")
-                    self.chat_history.insert(tk.END, 155 * "=", "system_line")
-                    self.chat_history.insert(tk.END, "\n", "system_line")
-                    self.chat_history.configure(state="disabled")
-
-                    if self.history_var.get():
-                        self.write_history(user_input, response)
-
-                    self.input_entry.delete("1.0", "end")  # Очистка поля ввода
         except Exception as e:
             messagebox.showerror("Возникла ошибка", e)
 
@@ -436,6 +418,15 @@ class ChatApp(tk.Tk):
             return "break"
         except Exception as e:
             messagebox.showerror("Возникла ошибка", e)
+
+    def toggle_theme(self):
+        current_theme = ctk.get_appearance_mode()
+        if current_theme == "Dark":
+            ctk.set_appearance_mode("light")
+            self.theme_button.configure(text="Тёмная тема")
+        else:
+            ctk.set_appearance_mode("dark")
+            self.theme_button.configure(text="Светлая тема")
 
 if __name__ == "__main__":
     check_for_updates()
